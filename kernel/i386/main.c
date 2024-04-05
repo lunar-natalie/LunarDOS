89b7e4c707c19be63e178b7e34a0a4f8646e2675
Natalie Wiggins <islifepeachy@outlook.com>
2024-04-05 16:24:37 +0100

    Initial commit

---
+++ kernel/i386/main.c
@@ -0,0 +1,12 @@
+/*
+ * main.s
+ * Kernel entry point
+ *
+ * Copyright (c) 2023 Natalie Wiggins <islifepeachy@outlook.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+void kernel_main(void)
+{
+}
---
+++ kernel/i386/start.s
@@ -0,0 +1,45 @@
+/*
+ * start.s
+ * Multiboot v1 compatible bootstrap assembly
+ *
+ * Copyright (c) 2023 Natalie Wiggins <islifepeachy@outlook.com>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+/* Multiboot constants */
+.set ALIGN,	1 << 0
+.set MEMINFO,	1 << 1
+.set FLAGS,	ALIGN | MEMINFO
+.set MAGIC,	0x1BADB002
+.set CHECKSUM,	-(MAGIC + FLAGS)
+
+/* Multiboot header */
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+/* Stack (16K) */
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384
+stack_top:
+
+/* Entrypoint */
+.section .text
+.global _start
+.type _start, @function
+_start:
+	mov	$stack_top, %esp
+	call	kernel_main
+
+.size _start, . - _start	/* Set symbol size for debugging */
+
+/* Stop execution */
+_end:
+	cli
+1:	hlt			/* Halt on NMI */
+	jmp	1b
