82db376d32005f6d62f76d45ed6dba1902d9c127
Natalie Wiggins <islifepeachy@outlook.com>
2024-04-06 15:03:21 +0100

    Add kernel version

---
+++ kernel/version.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 Natalie Wiggins. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+typedef struct {
+    int major;
+    int minor;
+    int patch;
+} version_t;
+
+static const version_t KERNEL_VERSION;
