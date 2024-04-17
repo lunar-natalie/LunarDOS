a98dea52b017dd35f1ca4e2643ea61e9efdec101
Natalie Wiggins <islifepeachy@outlook.com>
2024-04-17 11:57:40 +0100

    Update comments and uncomment stdio demo

--- kernel/i386/main.c
+++ kernel/i386/main.c
@@ -15,18 +15,18 @@ static tss_t tss;
 
 void kernel_main(void)
 {
-    // TODO: Fix GDT and far jump to kernel code segment
-    // TODO: Verify TSS
-    // TODO: Fully implement interrupts
+    // TODO: Fix GDT in order to far jump to kernel code segment
+    // TODO: Verify TSS implementation
+    // TODO: Implement interrupts
     init_paging();
     init_gdt(&tss);
 //    init_tss(&tss);
 //    load_tss(GDT_INDEX_RING0_TSS, 0);
 //    init_idt();
-//    init_tty();
+    init_tty();
 
     // TODO: Test exception handling when the required components are implemented
 //    test();
 
-//    printf("Loaded\n");
+    printf("Loaded\n");
 }
