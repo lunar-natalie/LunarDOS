1a3a1d859ca9e8b159923a1da95941b3fc15446b
Natalie Wiggins <islifepeachy@outlook.com>
2024-04-21 23:35:12 +0100

    Add defs.h

---
+++ include/kernel/defs.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Natalie Wiggins. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+typedef int error_t;
+enum {
+    ERROR_SUCCESS = 0,
+    ERROR_FAILURE = -1
+};
